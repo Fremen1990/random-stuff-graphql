3904857d2c8c949b9dc46cffc68b41bd25dcd5ff
DevThomas <fremen1990@gmail.com>
2022-06-28 20:50:09 +0200

    Create a simple graphQL server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello WORLD!!????")
+const {ApolloServer} = require('apollo-server')
+
+const typeDefs = `
+type Query {
+greeting: String
+}
+`;
+
+    const server = new ApolloServer({typeDefs});
+
+    server.listen({port: 4000}).then(result=> console.log("Apollo is running on: ", result.url))
